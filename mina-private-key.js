dad0039904b92964340072d3b855b2581cce2d49
zinderud <mokosam@gmail.com>
2024-12-20 15:06:24 +0300

    fixed    // Mina özel anahtar ön eki ekleme

--- mina-private-key.js
+++ mina-private-key.js
@@ -1,34 +1,49 @@
-const bip39 = require("bip39");
-const bip32 = require("bip32");
+import { Buffer } from 'safe-buffer';
+import bs58check from 'bs58check';
+import bip39 from 'bip39';
+import bip32 from 'bip32';
+// Kullanım
+const mnemonic = 'mnomonicler buraya yazılacak';
+const derivationPath = "m/44'/12586'/0'/0";
 
-// Mnemonic kelimelerinizi buraya ekleyin
-const mnemonic = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about";
-
-// Mina derivation path
-const derivationPath = "m/44'/12586'/0'/0/0";
-
-// Mnemonic'ten Private Key türetme
-async function getMinaPrivateKey(mnemonic, path) {
-  try {
-    // Seed oluşturma
-    const seed = await bip39.mnemonicToSeed(mnemonic);
-    console.log("Mina Seed:", seed);
-    
-    // Root key oluşturma
-    const root = bip32.fromSeed(seed);
-    
-    // Derivation path'i takip ederek key oluşturma
-    const child = root.derivePath(path);
-    
-    // Private Key'i hexadecimal olarak döndür
-    const privateKey = child.privateKey.toString("hex");
-    
-    console.log("Mina Private Key:", privateKey);
-    return privateKey;
-  } catch (error) {
-    console.error("Hata:", error);
+function reverse(bytes) {
+  const reversed = new Buffer(bytes.length);
+  for (let i = bytes.length; i > 0; i--) {
+      reversed[bytes.length - i] = bytes[i - 1];
   }
+  return reversed;
+}
+async function generateMinaPrivateKey(mnemonic, derivationPath) {
+  if (!bip39.validateMnemonic(mnemonic)) {
+    throw new Error('Geçersiz mnemonic kelimeler.');
+  }
+
+  // Mnemonic'ten seed oluşturma
+  const seed = await bip39.mnemonicToSeed(mnemonic);
+  const root = bip32.fromSeed(seed);
+
+  // Mina derivation path takip edilerek child private key türetilir
+  const child = root.derivePath(derivationPath);
+      // İlk baytın maskeleme işlemi
+      child.privateKey[0] &= 0x3f;
+      const reversedPrivateKey = reverse(child.privateKey);
+
+  // Mina özel anahtar ön eki ekleme
+  const privateKeyHex = `5a01${reversedPrivateKey.toString('hex')}`;
+
+  // BASE58CHECK formatına dönüştürme
+  const privateKey = bs58check.encode(Buffer.from(privateKeyHex, 'hex'));
+
+  console.log('Mina Private Key (BASE58):', privateKey);
+  return privateKey;
 }
 
-// Private Key'i almak için fonksiyonu çağır
-getMinaPrivateKey(mnemonic, derivationPath);
+
+
+generateMinaPrivateKey(mnemonic, derivationPath)
+  .then((privateKey) => {
+    console.log('Türetilen Private Key:', privateKey);
+  })
+  .catch((error) => {
+    console.error('Hata:', error.message);
+  });
